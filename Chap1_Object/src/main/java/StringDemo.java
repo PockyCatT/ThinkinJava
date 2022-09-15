99e6353dd5dc5cd4e449e795437903ab09866da8
yuanye <2950225124@qq.com>
2022-09-15 11:15:15 +0800

    Demo update at 2022.9.15

---
+++ Chap1_Object/src/main/java/StringDemo.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2021-2022, yuanyeaf
+ * Date: 2022/9/15 11:04
+ * FileName: StringDemo
+ * Description: 34432
+ * Version: 1.0
+ */
+
+/**
+ * @Author: YuanYe
+ * @CreateTime: 2022/9/15 11:04
+ * @Description: TODO
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        String x = "231231";
+        String y = "231231";
+        // 默认缓存池, true
+        System.out.println(x==y);
+
+        String m = new String("123123");
+        String n = new String("123123");
+        // 新的堆实例, false
+        System.out.println(m==n);
+    }
+}
